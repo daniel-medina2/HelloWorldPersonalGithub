523f54286bf588094f60901521adf008a58d4216
Daniel Medina <dmedina2@zagmail.gonzaga.edu>
2025-01-26 11:19:17 -0800

    initial

--- HelloWorldPersonalGithub/main.py
+++ HelloWorldPersonalGithub/main.py
@@ -1,4 +1,7 @@
 
 # Github Practice (Lecture 2)
 
+# https://github.com/daniel-medina2/HelloWorldPersonalGithub.git
+# https://github.com/daniel-medina2/HelloWorldPersonalGithub.git
+
 print("hello")
